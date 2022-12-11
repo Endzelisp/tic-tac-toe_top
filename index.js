5e7f4b69fe63548ac5ae8de798f5a662bb190d15
Endzelisp <endzelisp@gmail.com>
2022-12-11 15:00:30 -0400

    Simplify winner method on Gameplay module

--- index.js
+++ index.js
@@ -44,14 +44,21 @@ const Gameplay = ( function (board) {
 
   const winner = function () {
     const [p0, p1, p2, p3, p4, p5, p6, p7 ,p8] = board;
-    if ((p0.mark === 'X' || p0.mark === 'O') && (p0.mark === p1.mark) && (p1.mark === p2.mark)) {return p0.mark}
-    if ((p3.mark === 'X' || p3.mark === 'O') && (p3.mark === p4.mark) && (p4.mark === p5.mark)) {return p3.mark}
-    if ((p6.mark === 'X' || p6.mark === 'O') && (p6.mark === p7.mark) && (p7.mark === p8.mark)) {return p6.mark}
-    if ((p0.mark === 'X' || p0.mark === 'O') && (p0.mark === p3.mark) && (p3.mark === p6.mark)) {return p0.mark}
-    if ((p1.mark === 'X' || p1.mark === 'O') && (p1.mark === p4.mark) && (p4.mark === p7.mark)) {return p1.mark}
-    if ((p2.mark === 'X' || p2.mark === 'O') && (p2.mark === p5.mark) && (p5.mark === p8.mark)) {return p6.mark}
-    if ((p0.mark === 'X' || p0.mark === 'O') && (p0.mark === p4.mark) && (p4.mark === p8.mark)) {return p0.mark}
-    if ((p2.mark === 'X' || p2.mark === 'O') && (p2.mark === p4.mark) && (p4.mark === p6.mark)) {return p2.mark}
+
+    function check (a, b, c) {
+      if (a === 'X' || a === 'O') {
+        return (a === b) && (b === c)
+      }
+    }
+
+    if (check(p0.mark, p1.mark, p2.mark)) return p0.mark
+    if (check(p3.mark, p4.mark, p5.mark)) return p3.mark
+    if (check(p6.mark, p7.mark, p8.mark)) return p6.mark
+    if (check(p0.mark, p3.mark, p6.mark)) return p0.mark
+    if (check(p1.mark, p4.mark, p7.mark)) return p1.mark
+    if (check(p2.mark, p5.mark, p8.mark)) return p6.mark
+    if (check(p0.mark, p4.mark, p8.mark)) return p0.mark
+    if (check(p2.mark, p4.mark, p6.mark)) return p2.mark
   }
 
   return {activePlayer, isOver, winner}
